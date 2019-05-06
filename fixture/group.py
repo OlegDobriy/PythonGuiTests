727f4b7bd26423ebc8c339b1cb5c18426c33fca3
OlegDobriy <46223141+OlegDobriy@users.noreply.github.com>
2019-05-06 22:21:54 +0300

    Тест для удаления группы и методы для этого

--- fixture/group.py
+++ fixture/group.py
@@ -2,10 +2,12 @@ class GroupHelper:
 
     def __init__(self, app):
         self.app = app
+        self.group_editor = self.app.application.window(title='Group editor')
+        self.delete_group = self.app.application.window(title='Delete group')
 
     def get_groups_list(self):
         self.open_group_editor()
-        groups_tree = self.group_editor.window(auto_id='uxAddressTreeView')  # найти список элемент списка групп
+        groups_tree = self.group_editor.child_window(auto_id='uxAddressTreeView')  # найти список элемент списка групп
         root = groups_tree.tree_root()  # найти корневой элемент
         groups_list = [node.text() for node in root.children()]  # получить список групп
         self.close_group_editor()
@@ -13,24 +15,22 @@ class GroupHelper:
 
     def create(self, name):
         self.open_group_editor()
-        self.group_editor.window(auto_id='uxNewAddressButton').click()
-        input_field = self.group_editor.window(class_name='Edit')
+        self.group_editor.child_window(auto_id='uxNewAddressButton').click()
+        input_field = self.group_editor.child_window(class_name='Edit')
         input_field.set_text(name)
         input_field.type_keys('\n')
 
     def delete(self, group):
         self.open_group_editor()
-        self.group_editor.window(class_name='%s' % group).click()
-        self.group_editor.window(auto_id='uxDeleteAddressButton')
-        self.group_editor.window(auto_id='uxDeleteAllRadioButton')
-        self.group_editor.window(auto_id='uxOKAddressButton')
-        self.close_group_editor()
-
+        groups_tree = self.group_editor.child_window(auto_id='uxAddressTreeView')  # найти список элемент списка групп
+        groups_tree.get_item([u'Contact groups', u'%s' % group]).click()
+        self.group_editor.child_window(auto_id='uxDeleteAddressButton').click()
+        self.delete_group.child_window(auto_id='uxDeleteAllRadioButton').click()
+        self.delete_group.child_window(auto_id='uxOKAddressButton').click()
         self.close_group_editor()
 
     def open_group_editor(self):
-        self.app.main_window.window(auto_id='groupButton').click()
-        self.group_editor = self.app.application.window(title='Group editor')
+        self.app.main_window.child_window(auto_id='groupButton').click()
         self.group_editor.wait('visible')
 
     def close_group_editor(self):
--- test/test_delete_group.py
+++ test/test_delete_group.py
@@ -1,6 +1,9 @@
 import random
 
+
 def test_delete_group(app):
+    if len(app.group.get_groups_list()) <= 1:
+        app.group.create('name_test_delete_group')
     old_groups_list = app.group.get_groups_list()
     group = random.choice(old_groups_list)
     app.group.delete(group)
