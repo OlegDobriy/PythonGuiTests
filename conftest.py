ed2c016f6c4383dca8eadc650147d559fb9cc91c
OlegDobriy <46223141+OlegDobriy@users.noreply.github.com>
2019-05-07 22:56:26 +0300

    Параметризация теста добавления группы

--- conftest.py
+++ conftest.py
@@ -1,5 +1,7 @@
 import pytest
 from fixture.application import Application
+from comtypes.client import CreateObject
+import os.path
 
 
 @pytest.fixture(scope='session')
@@ -7,3 +9,17 @@ def app(request):
     fixture = Application('D:\\AddressBookPortable\\AddressBook.exe')
     request.addfinalizer(fixture.destroy)
     return fixture
+
+
+def pytest_generate_tests(metafunc):
+    for fixture in metafunc.fixturenames:  # тестовые данные
+        if fixture.startswith('data_'):
+            xl = CreateObject("Excel.Application")
+            wb = xl.Workbooks.Open(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'data/%s.xlsx' % fixture[5:]))
+            worksheet = wb.Sheets[1]
+            data_list = []
+            for row in range(1, 11):
+                data = worksheet.Cells[row, 1].Value()
+                data_list.append(data)
+            xl.Quit()
+            metafunc.parametrize(fixture, data_list, ids=[str(x) for x in data_list])
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,6 +1,7 @@
-def test_add_group(app):
+def test_add_group(app, data_groups):
+    group = data_groups
     old_groups_list = app.group.get_groups_list()
-    app.group.create('test_add_group')
+    app.group.create(group)
     new_groups_list = app.group.get_groups_list()
-    old_groups_list.append('test_add_group')
+    old_groups_list.append(group)
     assert sorted(old_groups_list) == sorted(new_groups_list)
